b3a5604e71e85b5fbaaa63a31ebd8f443818a028
Andrei Razhkou <andreirazhkou@gmail.com>
2025-05-15 13:02:02 +0300

    leetcode

---
+++ src/_159_LongestSubstringWithAtMostTwoDistinctCharacters.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright
+ * Andrei Razhkou
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class _159_LongestSubstringWithAtMostTwoDistinctCharacters {
+
+    public static void main(String[] args) {
+        System.out.println(longestSubstr("eceba"));
+        System.out.println(longestSubstr("ccaabbb"));
+        System.out.println(longestSubstr("cab"));
+    }
+
+    public static int longestSubstr(String s) {
+        int maxLength = 0;
+
+        int left = 0;
+        int right = 0;
+        Map<Character, Integer> chars = new HashMap<>();
+
+        while (right < s.length()) {
+            char rightChar = s.charAt(right);
+            if (!chars.containsKey(rightChar)) {
+                while (chars.size() == 2) {
+                    if (chars.get(s.charAt(left)) == 1) {
+                        chars.remove(s.charAt(left));
+                    } else {
+                        chars.put(s.charAt(left), chars.get(s.charAt(left)) - 1);
+                    }
+                    left++;
+                }
+            }
+            chars.put(rightChar, chars.computeIfAbsent(rightChar, k -> 0) + 1);
+            maxLength = Math.max(maxLength, right - left + 1);
+            right++;
+        }
+
+        return maxLength;
+    }
+}
